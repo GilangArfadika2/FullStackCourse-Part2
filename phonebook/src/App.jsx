097a4db5fb870e051708893f57510cce08bdc572
gilang arfadika <gilang.arfadika@ui.ac.id>
2025-06-29 13:33:20 +0700

    finish exercise 2.11

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -1,18 +1,32 @@
 
-import { useState } from 'react'
-import Note from './components/Note'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import PersonForm from './components/PersonForm'
 import PersonData from './components/PersonData'
 import FilterForm from './components/FilterForm'
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  // const [persons, setPersons] = useState([
+  //   { name: 'Arto Hellas', number: '040-123456', id: 1 },
+  //   { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+  //   { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+  //   { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  // ]) 
+  const [persons,setPersons] = useState([])
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }
+  
+  useEffect(hook, [])
+
+  
   const [newName, setNewName] = useState('')
   const [newPhoneNumber , setNewPhoneNumber] = useState('')
   const [filterInput, setFilterInput] = useState('')
